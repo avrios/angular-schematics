d2590922ccca847ee7d23c8a3ab1fa5f87c6a0c1
Vladimir Zdenek <ydenek.v@gmail.com>
2020-05-19 09:24:18 +0200

    feat: add a "page-component" schematic

---
+++ src/page-component/index.js
@@ -0,0 +1,44 @@
+"use strict";
+Object.defineProperty(exports, "__esModule", { value: true });
+const core_1 = require("@angular-devkit/core");
+const schematics_1 = require("@angular-devkit/schematics");
+const parse_name_1 = require("../utilities/parse-name");
+const project_1 = require("../utilities/project");
+const validation_1 = require("../utilities/validation");
+function buildSelector(options, projectPrefix) {
+    let selector = core_1.strings.dasherize(options.name);
+    if (options.prefix) {
+        selector = `${options.prefix}-${selector}`;
+    }
+    else if (options.prefix === undefined && projectPrefix) {
+        selector = `${projectPrefix}-${selector}`;
+    }
+    return selector;
+}
+function default_1(options) {
+    return (host) => {
+        if (!options.project) {
+            throw new schematics_1.SchematicsException('Option (project) is required.');
+        }
+        const project = project_1.getProject(host, options.project);
+        if (options.path === undefined) {
+            options.path = project_1.buildDefaultPath(project);
+        }
+        const parsedPath = parse_name_1.parseName(options.path, options.name);
+        options.name = parsedPath.name;
+        options.path = parsedPath.path;
+        options.selector = options.selector || buildSelector(options, project.prefix);
+        validation_1.validateName(options.name);
+        validation_1.validateHtmlSelector(options.selector);
+        const templateSource = schematics_1.apply(schematics_1.url('./files'), [
+            options.skipTests ? schematics_1.filter(path => !path.endsWith('.spec.ts.template')) : schematics_1.noop(),
+            schematics_1.applyTemplates(Object.assign({}, core_1.strings, options)),
+            schematics_1.move(parsedPath.path),
+        ]);
+        return schematics_1.chain([
+            schematics_1.mergeWith(templateSource)
+        ]);
+    };
+}
+exports.default = default_1;
+//# sourceMappingURL=index.js.map
---
+++ src/page-component/index.ts
@@ -0,0 +1,73 @@
+import { strings } from '@angular-devkit/core';
+import {
+    Rule,
+    SchematicsException,
+    Tree,
+    apply,
+    applyTemplates,
+    chain,
+    mergeWith,
+    move,
+    url,
+    filter,
+    noop
+} from '@angular-devkit/schematics';
+
+import { parseName } from '../utilities/parse-name';
+import { buildDefaultPath, getProject } from '../utilities/project';
+import { validateHtmlSelector, validateName } from '../utilities/validation';
+
+interface ComponentOptions {
+    name: string;
+    path: string;
+    prefix?: string;
+    project: string;
+    selector?: string;
+    skipTests?: boolean;
+}
+
+function buildSelector(options: ComponentOptions, projectPrefix: string) {
+    let selector = strings.dasherize(options.name);
+    if (options.prefix) {
+        selector = `${options.prefix}-${selector}`;
+    } else if (options.prefix === undefined && projectPrefix) {
+        selector = `${projectPrefix}-${selector}`;
+    }
+
+    return selector;
+}
+
+export default function (options: ComponentOptions): Rule {
+    return (host: Tree) => {
+        if (!options.project) {
+            throw new SchematicsException('Option (project) is required.');
+        }
+
+        const project = getProject(host, options.project);
+
+        if (options.path === undefined) {
+            options.path = buildDefaultPath(project);
+        }
+
+        const parsedPath = parseName(options.path, options.name);
+        options.name = parsedPath.name;
+        options.path = parsedPath.path;
+        options.selector = options.selector || buildSelector(options, project.prefix);
+
+        validateName(options.name);
+        validateHtmlSelector(options.selector);
+
+        const templateSource = apply(url('./files'), [
+            options.skipTests ? filter(path => !path.endsWith('.spec.ts.template')) : noop(),
+            applyTemplates({
+                ...strings,
+                ...options,
+            }),
+            move(parsedPath.path),
+        ]);
+
+        return chain([
+            mergeWith(templateSource)
+        ]);
+    };
+}
