b044fdfab0384dfe6727fb16b7a0f9beba4610bd
Jessica Gomez <jessica.gomez@avrios.com>
2022-08-16 15:06:21 +0200

    feat: Add schematic for improving and automating libraries creation
    JIRA-Ticket: AV-24340

---
+++ src/library/index.js
@@ -0,0 +1,81 @@
+"use strict";
+Object.defineProperty(exports, "__esModule", { value: true });
+const schematics_1 = require("@angular-devkit/schematics");
+const parse_name_1 = require("../utilities/parse-name");
+const project_1 = require("../utilities/project");
+const validation_1 = require("../utilities/validation");
+function default_1(options) {
+    const angularConfigFile = '/angular.json';
+    const nxConfigFile = '/nx.json';
+    return (host) => {
+        if (!options.project) {
+            throw new schematics_1.SchematicsException('Option (project) is required.');
+        }
+        const project = project_1.getProject(host, options.project);
+        if (options.path === undefined) {
+            options.path = project_1.buildDefaultPath(project);
+        }
+        const parsedPath = parse_name_1.parseName(options.path, options.name);
+        options.name = parsedPath.name;
+        options.path = parsedPath.path;
+        validation_1.validateName(options.name);
+        const workspaceConfig = host.read(angularConfigFile);
+        const nxWorkspaceConfig = host.read(nxConfigFile);
+        if (!workspaceConfig) {
+            throw new schematics_1.SchematicsException('Could not find Angular worspace configuration.');
+        }
+        if (!nxWorkspaceConfig) {
+            throw new schematics_1.SchematicsException('Could not find NX worspace configuration.');
+        }
+        const workspaceContent = workspaceConfig.toString();
+        const nxWorkspaceContent = nxWorkspaceConfig.toString();
+        const workspace = JSON.parse(workspaceContent);
+        const nxWorkspace = JSON.parse(nxWorkspaceContent);
+        workspace.projects[options.name] = {
+            root: options.path,
+            sourceRoot: options.path + '/src',
+            projectType: 'library',
+            schematics: {},
+            prefix: options.prefix || 'avr',
+            architect: {
+                lint: {
+                    builder: '@angular-devkit/build-angular:tslint',
+                    options: {
+                        tsConfig: [
+                            options.path + '/tsconfig.lib.json',
+                            options.path + '/tsconfig.spec.json',
+                            options.path + '.storybook/tsconfig.json' // libs/shared/.storybook/tsconfig.json
+                        ],
+                        exclude: [
+                            '**/node_modules/**',
+                            '!' + options.path + '/**/*' // !libs/shared/**/*
+                        ]
+                    }
+                },
+                test: {
+                    builder: '@nrwl/jest:jest',
+                    options: {
+                        jestConfig: options.path + '/jest.config.js',
+                        passWithNoTests: true
+                    }
+                }
+            }
+        };
+        nxWorkspace.projects[options.name] = {
+            tags: []
+        };
+        host.overwrite(angularConfigFile, JSON.stringify(workspace));
+        host.overwrite(nxConfigFile, JSON.stringify(nxWorkspace));
+        // const templateSource = apply(url('./files'), [
+        //     options.skipTests ? filter(path => !path.endsWith('.spec.ts.template')) : noop(),
+        //     applyTemplates({
+        //         ...strings,
+        //         ...options,
+        //     }),
+        //     move(parsedPath.path),
+        // ]);
+        return host;
+    };
+}
+exports.default = default_1;
+//# sourceMappingURL=index.js.map
---
+++ src/library/index.ts
@@ -0,0 +1,123 @@
+import { experimental } from '@angular-devkit/core';
+import {
+    Rule,
+    SchematicsException,
+    Tree
+} from '@angular-devkit/schematics';
+
+import { parseName } from '../utilities/parse-name';
+import { buildDefaultPath, getProject } from '../utilities/project';
+import { validateName } from '../utilities/validation';
+
+interface LibraryOptions {
+    name: string;
+    path: string;
+    project: string;
+    prefix: string;
+    skipTests?: boolean;
+}
+
+interface NxWorkspaceSchema {
+    npmScope: string;
+    affected: {
+      [key:string]: any;
+    },
+    implicitDependencies: {
+        [key:string]: any;
+    },
+    tasksRunnerOptions: {
+        [key:string]: any;
+    },
+    projects: {
+        [key:string]: any;
+    }
+}
+
+export default function (options: LibraryOptions): Rule {
+    const angularConfigFile = '/angular.json';
+    const nxConfigFile = '/nx.json';
+
+    return (host: Tree) => {
+        if (!options.project) {
+            throw new SchematicsException('Option (project) is required.');
+        }
+
+        const project = getProject(host, options.project);
+
+        if (options.path === undefined) {
+            options.path = buildDefaultPath(project);
+        }
+
+        const parsedPath = parseName(options.path, options.name);
+        options.name = parsedPath.name;
+        options.path = parsedPath.path;
+
+        validateName(options.name);
+
+        const workspaceConfig = host.read(angularConfigFile);
+        const nxWorkspaceConfig = host.read(nxConfigFile);
+
+        if (!workspaceConfig) {
+            throw new SchematicsException('Could not find Angular worspace configuration.');
+        }
+
+        if (!nxWorkspaceConfig) {
+            throw new SchematicsException('Could not find NX worspace configuration.');
+        }
+
+        const workspaceContent = workspaceConfig.toString();
+        const nxWorkspaceContent = nxWorkspaceConfig.toString();
+
+        const workspace: experimental.workspace.WorkspaceSchema = JSON.parse(workspaceContent);
+        const nxWorkspace: NxWorkspaceSchema = JSON.parse(nxWorkspaceContent);
+
+        workspace.projects[options.name] = {
+            root: options.path, // libs/shared,
+            sourceRoot: options.path + '/src', // libs/shared/src,
+            projectType: 'library',
+            schematics: {},
+            prefix: options.prefix || 'avr',
+            architect: {
+              lint: {
+                builder: '@angular-devkit/build-angular:tslint',
+                options: {
+                  tsConfig: [
+                    options.path + '/tsconfig.lib.json', // libs/shared/tsconfig.lib.json,
+                    options.path + '/tsconfig.spec.json', //libs/shared/tsconfig.spec.json,
+                    options.path + '.storybook/tsconfig.json' // libs/shared/.storybook/tsconfig.json
+                  ],
+                  exclude: [
+                    '**/node_modules/**',
+                    '!' + options.path + '/**/*'// !libs/shared/**/*
+                  ]
+                }
+              },
+              test: {
+                builder: '@nrwl/jest:jest',
+                options: {
+                  jestConfig: options.path + '/jest.config.js', // libs/shared/jest.config.js,
+                  passWithNoTests: true
+                }
+              }
+            }
+        };
+
+        nxWorkspace.projects[options.name] = {
+            tags: []
+        }
+
+        host.overwrite(angularConfigFile, JSON.stringify(workspace));
+        host.overwrite(nxConfigFile, JSON.stringify(nxWorkspace));
+
+        // const templateSource = apply(url('./files'), [
+        //     options.skipTests ? filter(path => !path.endsWith('.spec.ts.template')) : noop(),
+        //     applyTemplates({
+        //         ...strings,
+        //         ...options,
+        //     }),
+        //     move(parsedPath.path),
+        // ]);
+
+        return host;
+    };
+}
